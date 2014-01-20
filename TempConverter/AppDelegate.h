d0701ea9f880c52c3b19cd9a1f1ebe615f7af83c
Jenny Kwan <jennylkwan@gmail.com>
2014-01-19 22:16:12 -0800

    Initial Commit

---
+++ TempConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TempConverter
+//
+//  Created by Jenny Kwan on 1/19/14.
+//  Copyright (c) 2014 Jenny Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TempConverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TempConverter
+//
+//  Created by Jenny Kwan on 1/19/14.
+//  Copyright (c) 2014 Jenny Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
